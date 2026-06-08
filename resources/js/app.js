91c0dfbbb4c525cce192fe4e5cb6941a9b1b7963
Hnin Yu Shwe <140038221+hninyushwee@users.noreply.github.com>
2026-06-08 16:24:35 +0630

    Update app.js with Vue Router setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,42 @@
-import './bootstrap';
-import { createApp } from 'vue';
+import './bootstrap'
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-import App from './components/Navbar.vue';
+import App from './App.vue'
 
-createApp(App).mount('#app');
+// Import page components
+import Home from './components/user/Home.vue'
+
+// Create router
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: () => import('./pages/NotFound.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
+})
+
+const app = createApp(App)
+
+// Use router
+app.use(router)
+
+// Mount app
+app.mount('#app')
